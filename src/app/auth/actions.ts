18c0e63e92be7dd5f2ba4c9de9d553fc66579e7c
Thonybaquer <anthonybr201@gmail.com>
2026-02-13 01:42:26 -0500

    Delete src directory

--- src/app/auth/actions.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { clearSessionCookie } from '@/lib/auth';
-import { redirect } from 'next/navigation';
-
-export async function logoutAction() {
-  await clearSessionCookie();
-  redirect('/login');
-}
