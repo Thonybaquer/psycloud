ba1de6f2090dc2a5a765d39f3cba972e4d817d56
Thonybaquer <anthonybr201@gmail.com>
2026-02-12 23:55:18 -0500

    Delete electron directory

--- electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('psycloudDesktop', {
-  exportBackup: () => ipcRenderer.invoke('db:export'),
-  importBackup: () => ipcRenderer.invoke('db:import'),
-  enableDbEncryption: (args) => ipcRenderer.invoke('db:encrypt:enable', args),
-  disableDbEncryption: () => ipcRenderer.invoke('db:encrypt:disable'),
-  encryptionStatus: () => ipcRenderer.invoke('db:encrypt:status'),
-});
